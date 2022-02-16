e283de9dc116ed86d2e2331aa1de313735d49920
Hamad Bakeel <hamad200714@gmail.com>
2022-02-17 00:01:18 +0300

    updated the code sturcture using ejs

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const server = express();
+server.set('view engine', 'ejs')
+server.use(express.static(__dirname + '/public'));
+// server.use(express.static(__dirname + '/public/partials'));
+server.set('views', __dirname+'/views');
+server.listen(7000);
+
+const servicesIcons =[
+    'fas fa-user-shield fa-4x ',
+    'fas fa-tools fa-4x',
+    'fas fa-map-marked-alt fa-4x',
+    'fas fa-laptop-code fa-4x',
+    'fas fa-palette fa-4x',
+    'fas fa-bullhorn fa-4x'
+];
+const services =[
+    "Security",
+    "Fixing issues",
+    "Location",
+    "Proffisional Coding",
+    "Designing",
+    "Marketing"
+]
+server.get('/',(req, res)=>{
+    res.render('index',{servicesIcons,services});
+});
